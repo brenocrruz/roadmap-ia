42e70acf2497d572e9d38c9d17f934dc75eebf8e
brenocrruz <breno.cruz.dev@outlook.com>
2026-08-25 09:32:11 -0300

    estudando strings em python

--- caixa-1/gaveta-2/aulas/tipos-dinamicos/str.py
+++ caixa-1/gaveta-2/aulas/tipos-dinamicos/str.py
@@ -1,4 +1,13 @@
-texto = "Eu estou estudando"
-texto[0] = "T"
+texto = "Eu estou estudando".title()
+print(texto.upper())
 
+
+#texto[0] = "T"
 #print(f"texto: {id(texto)}, texto_2: {id(texto_2)}")
+
+
+"""
+lista = ("Banana", "Maça", "Laranja")
+print(f"Lista antes: {lista}")
+lista[0] = "Abacaxi"
+print(f"Lista depois: {lista}")"""
